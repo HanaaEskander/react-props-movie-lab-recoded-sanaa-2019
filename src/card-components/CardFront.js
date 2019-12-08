f8238a6881d78ac564c87f2b690faabcb93bea2e
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-08 08:34:54 +0000

    Automatically backed up by Learn

--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,12 +1,12 @@
-  
 import React, { Component } from 'react';
 
- class CardFront extends Component {
+export default class CardFront extends Component {
 
   render() {
     return (
+      
       <div className="card-front" style={{backgroundImage: `url(${this.props.poster})`}}>
       </div>
     )
   }
-export default CardFront;
+}
